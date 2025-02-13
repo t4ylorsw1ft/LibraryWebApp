6b9035c71160d8f7f58d4532cf0425e37adf9060
Иван Гриб <isgb194@gmail.com>
2025-02-13 21:08:02 +0300

    feat: added middleware for custom exception handling

---
+++ Library.WebApi/Middleware/CustomExceptionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Library.WebApi.Middleware
+{
+    public static class CustomExceptionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceptionHandler(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionHandlerMiddleware>();
+        }
+    }
+}

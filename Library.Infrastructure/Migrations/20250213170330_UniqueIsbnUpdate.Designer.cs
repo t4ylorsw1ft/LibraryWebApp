5d4d54d3f07b03a75999f802d8e80f78a3b3c643
Иван Гриб <isgb194@gmail.com>
2025-02-13 21:19:58 +0300

    feat: added new migration for database schema updates

--- Library.Infrastructure/Migrations/20250213170330_UniqueIsbnUpdate.Designer.cs
+++ Library.Infrastructure/Migrations/20250213170330_UniqueIsbnUpdate.Designer.cs
@@ -4,6 +4,7 @@ using Library.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Library.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250213170330_UniqueIsbnUpdate")]
+    partial class UniqueIsbnUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

8c9caeafda819ddda75c02ca8b1d005e7ebade0a
Иван Гриб <isgb194@gmail.com>
2025-02-13 21:07:03 +0300

    feat: added default value for ReturnBy field

--- Library.Domain/Entities/BookBorrow.cs
+++ Library.Domain/Entities/BookBorrow.cs
@@ -11,7 +11,7 @@ namespace Library.Domain.Entities
         public Guid Id { get; set; }
 
         public DateTime BorrowedAt { get; set; } = DateTime.UtcNow;
-        public DateTime ReturnBy { get; set; }
+        public DateTime ReturnBy { get; set; } = DateTime.UtcNow.AddDays(14);
         public bool IsReturned { get; set; } = false;
 
         public Guid BookId { get; set; }

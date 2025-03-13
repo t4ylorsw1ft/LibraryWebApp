4b1ce6be1a8bcda45da96b9d459a60eeb49c5729
Иван Гриб <isgb194@gmail.com>
2025-03-13 13:07:17 +0300

    ref: renamed "BookImagePath" field to "ImagePath" for consistency

--- Library.Application/UseCases/Books/DTOs/BookLookupDto.cs
+++ Library.Application/UseCases/Books/DTOs/BookLookupDto.cs
@@ -10,7 +10,7 @@ namespace Library.Application.UseCases.Books.DTOs
         public string Title { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
         public int AvaliableQuantity { get; set; }
-        public string? BookImagePath { get; set; }
+        public string? ImagePath { get; set; }
 
         public Guid AuthorId { get; set; }
         public string AuthorFirstName { get; set; } = string.Empty;
